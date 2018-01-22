2eb9b67f8c0da35ef8e9e7682640dd0d4dfeaf78
misCC <1518265554@qq.com>
2018-01-22 14:19:55 +0800

    Update

---
+++ NODE.md
@@ -0,0 +1,7 @@
+#Three 常用单词
+
+mash 网点
+
+Geometry 几何体
+
+vertex，vertices 顶点 // 线的两端
---
+++ index.js
@@ -0,0 +1,31 @@
+var renderer
+function initThree() {
+  
+}
+
+function initCamera() {
+  
+}
+
+function initScene() {
+  
+}
+
+function initLight() {
+  
+}
+
+function initObj() {
+  
+}
+
+function init() {
+  initThree()
+  initCamera()
+  initScene()
+  initLight()
+  initObj()
+  render()
+}
+
+init()
