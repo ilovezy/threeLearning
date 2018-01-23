42df35aafd63f81ec3adc0b82e6ae26b84f57a41
misCC <1518265554@qq.com>
2018-01-23 09:39:51 +0800

    split file

---
+++ material.js
@@ -0,0 +1,24 @@
+
+var pinkMat = new THREE.MeshPhongMaterial({
+  color: 0xdc5f45,//0xb43b29,//0xff5b49,
+  shininess: 0,
+  shading: THREE.FlatShading,
+});
+var blackMat = new THREE.MeshPhongMaterial({
+  color: 0x100707,
+  shading: THREE.FlatShading,
+});
+var brownMat = new THREE.MeshPhongMaterial({
+  color: 0xb44b39,
+  shininess: 0,
+  shading: THREE.FlatShading,
+});
+// 一些基本几何形的颜色
+var whiteMat = new THREE.MeshPhongMaterial({
+  color: 0xa49789,
+  shading: THREE.FlatShading,
+});
+var lightBrownMat = new THREE.MeshPhongMaterial({
+  color: 0xe07a57,
+  shading: THREE.FlatShading,
+});
